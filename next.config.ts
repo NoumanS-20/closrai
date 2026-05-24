4b265ee32d1a8f58c8b4f1f845826b6266324f96
Nouman Shafique <noumanahmed6860@gmail.com>
2026-05-24 10:52:50 +0530

    feat: update Next.js configuration and TypeScript settings; add error JSON file for agent failure details

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: "node_modules/.cache/next-build",
 };
 
 export default nextConfig;
